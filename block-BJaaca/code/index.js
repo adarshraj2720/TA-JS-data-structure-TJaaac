f7bec2f39f368d21b4b7d2009f980d9ae9fa3665
Adarsh Raj <adarshraj2721@gmail.com>
2022-01-11 17:15:18 +0530

    block-BJaaca

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -28,14 +28,22 @@ character["sur name"]="stark";
 character.title="Lady of Winterfell";
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 character.greet=`I am ${character.characterName} and my title is ${character.title}`
+
+character.greet=function(){
+    alert(`I am ${character.characterName} and my title is ${character.title}`);
+}
 // - Add a new property to the character object named `isFemale` and value will be `true`
 
 character.isFemale="true";
 // - Execute the function stored in the greet key of the character object
 
-character.greet;
+character.greet();
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
 character.greet=`she is ${character.characterName} and her title is ${character.title}`
+
+character.greet=function(){
+    alert(`I am ${character.characterName} and my title is ${character.title}`);
+}
 // - Check by calling `greet` and see if the message changed or not.
 
 character.greet;
@@ -46,9 +54,11 @@ if(age>18){
 }else{
     character.isAdult=`false`;
 }
+
+character.isAdult = character.age > 18 ?  `true` : `false`;
 // - Add a new method (function inside objects are called methods) named `changeIsAdult`. When called should be able to flip the value of `isAdult` from true to false and false to true.
-function changeIsAdult(){
-    charact
+character.changeIsAdult = function(){
+    character.isAdult= !character.isAdult;
 }
 // - Add a new key with the name stored in variable `keyName` defined below i.e `playedBy` (use [] for computed property) and the value of "Maisie Williams"
 
@@ -59,10 +69,10 @@ let keyName = 'playedBy';
  alert(character.keyName)
 // - Using the function `console.log` log the value of the key `42`. You don't have to add the key
 
-function keyNum (key=42){
-    console.log (key);
-}
+
+    console.log (character[42]);
+
 // - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
-character.totalSeasons=`1+2+3+2`
+character.totalSeasons=1+2+3+2;
 // - Access the value of the key `totalSeason` (it should be 8)
 character.totalSeasons;
